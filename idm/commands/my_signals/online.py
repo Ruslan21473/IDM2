27adc319e753173e63b1d790caec993b920f2823
Ruslan21473 <54320118+Ruslan21473@users.noreply.github.com>
2020-03-03 18:09:32 +0600

    Update online.py

--- idm/commands/my_signals/online.py
+++ idm/commands/my_signals/online.py
@@ -52,7 +52,7 @@ def off_online(event: MySignalEvent):
         return "ok"
     stop_thread = True
     # online_thread.join()
-    utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн остановлен.")
+    utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн остановлен")
     return "ok"
     
 
@@ -69,22 +69,22 @@ def on_online(event: MySignalEvent):
         utils.new_message(event.api, event.chat.peer_id, message=f"❗ Токен не установлен.\n Устанувить можно в админ-панеле https://{event.db.host}")
         return "ok"
     if online_thread != None and online_thread.is_alive():
-        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн  и так запущен.")
+        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн  и так запущен")
         return "ok"
     api_ = VkApi(token)
     online_thread = Thread(target=online_th, args=(api_, lambda:stop_thread))
     online_thread.setDaemon(True)
     online_thread.setName('Online Thread')
     online_thread.start()
-    utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн запущен.")
+    utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн запущен")
     return "ok"
 
 @dp.my_signal_event_handle('онлайн')
 def check_online(event: MySignalEvent):
     global online_thread
     if online_thread != None and online_thread.is_alive():
-        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн работает.")
+        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн работает")
         return "ok"
     else:
-        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн не работает.")
+        utils.new_message(event.api, event.chat.peer_id, message="✅ Вечный онлайн не работает")
         return "ok"
